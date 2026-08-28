1502b9486a12f0e53b300e20c7c29db805ef8f23
AungSoe Moe <61192036+whispermmepub@users.noreply.github.com>
2026-08-28 20:29:50 +0700

    Add shared iOS app links and trigger first Xcode build

---
+++ WoWReader/Support/AppConstants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum AppConstants {
+    static let telegramChannel = URL(string: "https://t.me/TheBookR")!
+    static let discussionGroup = URL(string: "https://t.me/+rUiqzi2mdhNiNGZl")!
+    static let bookWebsite = URL(string: "https://saroatsin.com")!
+    static let reviewWebsite = URL(string: "https://whispermmepub.github.io/Review/")!
+}
